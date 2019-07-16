b422e32d7fa1c688ef3e89f2192cb5626b74b3c0
gnzlbg <gonzalobg88@gmail.com>
2019-07-16 16:22:13 +0200

    merge nextafter

--- crates/libm/src/math/nextafter.rs
+++ crates/libm/src/math/nextafter.rs
@@ -1,6 +1,5 @@
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
-pub extern "C" fn nextafter(x: f64, y: f64) -> f64 {
+pub fn nextafter(x: f64, y: f64) -> f64 {
     if x.is_nan() || y.is_nan() {
         return x + y;
     }
--- crates/libm/src/math/nextafterf.rs
+++ crates/libm/src/math/nextafterf.rs
@@ -1,6 +1,5 @@
-#[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
-pub extern "C" fn nextafterf(x: f32, y: f32) -> f32 {
+pub fn nextafterf(x: f32, y: f32) -> f32 {
     if x.is_nan() || y.is_nan() {
         return x + y;
     }
